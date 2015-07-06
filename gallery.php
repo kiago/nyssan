150cc31a19ec5ac1d0cefe90741165f6258d9aeb
Kevin Agostinho <kikiago@gmail.com>
2015-07-06 22:02:44 +0200

    assets galerie

--- gallery.php
+++ gallery.php
@@ -7,15 +7,15 @@
 		<meta name="description" content="" />
 		<meta name="keywords" content="" />
 		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
-		<script src="js/jquery.min.js"></script>
-		<script src="js/jquery.poptrox.min.js"></script>
-		<script src="js/skel.min.js"></script>
-		<script src="js/init.js"></script>
+		<script src="assets_galerie/js/jquery.min.js"></script>
+		<script src="assets_galerie/js/jquery.poptrox.min.js"></script>
+		<script src="assets_galerie/js/skel.min.js"></script>
+		<script src="assets_galerie/js/init.js"></script>
 		<noscript>
-			<link rel="stylesheet" href="css/skel.css" />
-			<link rel="stylesheet" href="css/style.css" />
-			<link rel="stylesheet" href="css/style-desktop.css" />
-			<link rel="stylesheet" href="css/style-noscript.css" />
+			<link rel="stylesheet" href="assets_galerie/css/skel.css" />
+			<link rel="stylesheet" href="assets_galerie/css/style.css" />
+			<link rel="stylesheet" href="assets_galerie/css/style-desktop.css" />
+			<link rel="stylesheet" href="assets_galerie/css/style-noscript.css" />
 		</noscript>
 		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
 	</head>
